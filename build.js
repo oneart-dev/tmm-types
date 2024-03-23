de0916c7e91633483774540defc673eb9e198f57
Nick S <oart4k@gmail.com>
2024-03-23 09:20:22 -0500

    fix: move index.d.ts to root directory

--- build.js
+++ build.js
@@ -4,7 +4,7 @@ import { fileURLToPath } from "url";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url)); // For ES Module support to get __dirname
 const typesDir = path.join(__dirname, "./types");
-const indexFile = path.join(typesDir, "index.ts");
+const indexFile = path.join(__dirname, "index.d.ts");
 
 let exportStatements = "";
 
@@ -12,7 +12,7 @@ const files = fs.readdirSync(typesDir);
 files.forEach((file) => {
   if (file.endsWith(".ts") && file !== "index.ts") {
     const moduleName = path.basename(file, ".ts");
-    exportStatements += `export * from './${moduleName}';\n`;
+    exportStatements += `export * from './types/${moduleName}';\n`;
   }
 });
 
