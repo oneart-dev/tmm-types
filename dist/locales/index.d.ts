ed300c6c4a4188d0f2d0e39c02980c0477b7cd81
Nick S <oart4k@gmail.com>
2024-03-28 13:39:22 -0500

    build

--- dist/locales/index.d.ts
+++ dist/locales/index.d.ts
@@ -1,6 +1,8 @@
 declare enum Locales {
     en = "en",
-    ru = "ru"
+    ru = "ru",
+    ua = "ua",
+    es = "es"
 }
 export { Locales };
 //# sourceMappingURL=index.d.ts.map
--- dist/locales/index.js
+++ dist/locales/index.js
@@ -2,6 +2,8 @@ var Locales;
 (function (Locales) {
     Locales["en"] = "en";
     Locales["ru"] = "ru";
+    Locales["ua"] = "ua";
+    Locales["es"] = "es";
 })(Locales || (Locales = {}));
 export { Locales };
 //# sourceMappingURL=index.js.map
