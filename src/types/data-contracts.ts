571d588a1ab92332575da42c098d569caec2d0a9
Nick S <oart4k@gmail.com>
2026-06-03 11:59:09 -0500

    chore: update generated types

--- src/types/data-contracts.ts
+++ src/types/data-contracts.ts
@@ -3548,11 +3548,11 @@ export enum ServicesTagCategoryScope {
 
 /** @format int32 */
 export enum ServicesTagColumn {
-  TagCategoryCustomMin = 10,
-  TagCategoryCustomMax = 127,
   TagColumnEntryReason = 1,
   TagColumnExitReason = 2,
   TagColumnConclusion = 3,
+  TagCategoryCustomMin = 10,
+  TagCategoryCustomMax = 127,
 }
 
 export interface ServicesTagFilterGroup {
